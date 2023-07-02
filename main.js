46a6afdc5b6d6660568c18078b678aac4532d624
Railson Vieira <93285471+netmundo@users.noreply.github.com>
2023-07-01 21:45:33 -0300

    Chaychat
    Chatgptopenai

---
+++ main.js
@@ -0,0 +1,83 @@
+// Chave de API do OpenAI
+const apiKey = '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'
+
+function sendMessage(){
+    var message = document.getElementById('message-input')
+    if(!message.value)
+    {
+        message.style.border = '1px solid red'
+        return;
+    }
+    message.style.border = 'none'
+
+    var status = document.getElementById('status')
+    var btnSubmit = document.getElementById('btn-submit')
+
+    status.style.display = 'block'
+    status.innerHTML = 'Buscando...'
+    btnSubmit.disabled = true
+    btnSubmit.style.cursor = 'not-allowed'
+    message.disabled = true
+
+    fetch("https://api.openai.com/v1/completions",{
+        method: 'POST',
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: message.value,
+            max_tokens: 3000, // tamanho da resposta
+            temperature: 0.8 // criatividade na resposta
+        })
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        let r = response.choices[0]['text']
+        status.style.display = 'none'
+        showHistory(message.value,r)
+    })
+    .catch((e) => {
+        console.log(`Error -> ${e}`)
+        status.innerHTML = 'Erro, tente novamente mais tarde...'
+    })
+    .finally(() => {
+        btnSubmit.disabled = false
+        btnSubmit.style.cursor = 'pointer'
+        message.disabled = false
+        message.value = ''
+    })
+}
+
+function showHistory(message,response){
+    var historyBox = document.getElementById('history')
+
+    // My message
+    var boxMyMessage = document.createElement('div')
+    boxMyMessage.className = 'box-my-message'
+
+    var myMessage = document.createElement('p')
+    myMessage.className = 'my-message'
+    myMessage.innerHTML = message
+
+    boxMyMessage.appendChild(myMessage)
+
+    historyBox.appendChild(boxMyMessage)
+
+    // Response message
+    var boxResponseMessage = document.createElement('div')
+    boxResponseMessage.className = 'box-response-message'
+
+    var chatResponse = document.createElement('p')
+    chatResponse.className = 'response-message'
+    chatResponse.innerHTML = response
+
+    boxResponseMessage.appendChild(chatResponse)
+
+    historyBox.appendChild(boxResponseMessage)
+
+    // Levar scroll para o final
+    historyBox.scrollTop = historyBox.scrollHeight
+}
